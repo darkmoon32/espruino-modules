c69182da7e67d9a66cb40ff044a885205e4c9324
Tomáš Juřena <jurenatomas@gmail.com>
2016-07-21 14:07:08 +0200

    Custom encoder module

---
+++ MyEncoder.js
@@ -0,0 +1,23 @@
+	/* Copyright (c) 2013 Gordon Williams, Pur3 Ltd. See the file LICENSE for copying permission. */
+/*
+Module for connecting to Rotary encoder. 
+
+*/
+
+var step = 0;
+function MyEncoder(pina, pinb, holes) {
+  this.HOLES = holes * 2;
+  this.PINA = pina;
+  this.PINB = pinb;
+  this.Encoder = require("Encoder").connect(pina, pinb,function (direction) {
+    step++;
+  });
+}
+
+MyEncoder.prototype.getRounds = function(){
+  return Math.floor(step / this.HOLES);
+};
+
+exports.connect = function(pina, pinb, holes) {
+  return new MyEncoder(pina, pinb, holes);
+};
